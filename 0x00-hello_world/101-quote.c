d9d7872d5b9a85a6a5f06513f7425ecc89c9c817
eddiek001 <worksofeddy@gmail.com>
2023-09-07 20:49:57 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * * main - Entry point
- * *
- * * Return: Always 1 (Success)
- * */
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 		write(2,
