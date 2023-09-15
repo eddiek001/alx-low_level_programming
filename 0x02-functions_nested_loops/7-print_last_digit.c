8fbe0fad9424146ead39451e3998f79ecab0b5cb
eddiek001 <worksofeddy@gmail.com>
2023-09-15 11:57:15 +0000

    r

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,4 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-int r;
-
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
-}
+int main(void)}
