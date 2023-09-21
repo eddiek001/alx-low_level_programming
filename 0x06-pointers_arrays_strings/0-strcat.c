a66a6e2a3bb23a90b60a64912884316a2dc62e92
eddiek001 <worksofeddy@gmail.com>
2023-09-21 20:12:01 +0000

    join

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concantenates two strings
+ * @src: input values
+ * @dest: input value
+ *
+ * Return: (void)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int l;
+	i=0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	
+	}
+	
+	for (l = 0; scr[l] != '\0'; l++)
+	{
+	
+	}
+
+	for (i=0; i <= l; i++)
+	{
+		dest[i + i] = src[i];
+	}
+
+	return (dest);
