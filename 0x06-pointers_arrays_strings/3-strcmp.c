de16bdb430e94d6c5a36f29da7422efbc9fc338b
eddiek001 <worksofeddy@gmail.com>
2023-09-21 22:06:57 +0000

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: (0) if they are the same
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; s1[i] != '\0' && b == 0; i++)
+	{
+		j = s1[i] - s2[i];
+	}
+	return (b);
+}
