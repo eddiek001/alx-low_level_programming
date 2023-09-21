ec5a4385c36cd99beac43900e5b6f7fa6ca9a889
eddiek001 <worksofeddy@gmail.com>
2023-09-21 20:23:04 +0000

    join

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int s1, s2, i'
+	int s1, s2, i;
+
 	for (s1 = 0; dest[s1] != '\0'; s1++)
 	{
-	
 	}
-	
 	for (s2 = 0; scr[s2] != '\0'; s2++)
 	{
-	
 	}
-
-	for (i=0; i <= s2; i++)
+	for (i = 0; i <= s2; i++)
 	{
 		dest[s1 + i] = src[i];
 	}
-
 	return (dest);
