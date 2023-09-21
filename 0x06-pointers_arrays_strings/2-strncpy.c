0fbbedd6ef9a48d71bc44ac7a6881bcf000863e7
eddiek001 <worksofeddy@gmail.com>
2023-09-21 21:35:19 +0000

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: destination
+ * @src: source
+ * @n: no of string
+ * Return: return (dest)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest)
+}
