802d9d71146e08ae6f548f6fec9b9982b575455f
eddiek001 <worksofeddy@gmail.com>
2023-09-21 23:52:19 +0000

    j

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,12 @@ char *cap_string(char *str)
 {
 	int i, j;
 	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
-		for (i = 0; str[i] != '\0'; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-		str[i] = str[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	for (j = 0; c[j] != '0'; j++)
 	{
