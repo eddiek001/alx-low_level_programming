5010918323b3347597a9ae912e09aebff9c16019
eddiek001 <worksofeddy@gmail.com>
2023-09-21 22:10:34 +0000

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int j = 0;
 
-	for (i = 0; s1[i] != '\0' && b == 0; i++)
+	for (i = 0; s1[i] != '\0' && j == 0; i++)
 	{
 		j = s1[i] - s2[i];
 	}
-	return (b);
+	return (j);
 }
