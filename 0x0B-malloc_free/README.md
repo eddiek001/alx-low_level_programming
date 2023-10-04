9605605323402670b1f3a90a9e9c4e1567d92127
eddiek001 <worksofeddy@gmail.com>
2023-10-04 15:52:25 +0000

    create_array

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#0x0B. C - malloc, free
+Float like a butterfly, sting like a bee
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - character c
+ * @c: character to print
+ *
+ * Return: success (1)
+ * 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
