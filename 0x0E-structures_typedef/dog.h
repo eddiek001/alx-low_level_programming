6dfe8c697ed293f47680d1ee1c48081e721fa9c1
eddiek001 <worksofeddy@gmail.com>
2023-10-09 22:18:39 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - name, age and owner
+ * @name: first input
+ * @age: second input
+ * @owner: third input
+ *
+ * Description: Longer description
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
