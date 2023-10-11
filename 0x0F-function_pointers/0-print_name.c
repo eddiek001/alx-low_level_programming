001a91c2fc43d1d7247197f08e3fc44240aa5339
eddiek001 <worksofeddy@gmail.com>
2023-10-11 10:07:04 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
-/*
- * print_name - a function to print name
- * @f: variable to hold str
- * @name: name of the person
- */
-
+/**
+ * print_name - a function that  prints name
+ * @name: string to add
+ * @f: pointer to function variable
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
+
