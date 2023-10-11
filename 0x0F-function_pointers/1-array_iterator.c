30837f28bf8916d6de62be40af0548f723d0da61
eddiek001 <worksofeddy@gmail.com>
2023-10-11 10:32:39 -0700

    array interator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  * @size: size array#include <stdio.h
  * Return: void
  **/
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
