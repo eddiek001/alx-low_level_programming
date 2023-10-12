61c945211f332c893df477abe1bde0047f77d67e
eddiek001 <worksofeddy@gmail.com>
2023-10-12 13:36:05 +0000

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,8 +28,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		}
 		if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
+			printf("%s", separator);
 	}
-	printf("\n");
+	print("\n");
 	va_end(ptr_str);
 }
