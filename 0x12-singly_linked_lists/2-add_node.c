a15ca657b757eeecdbcde99c367e4da08a90b45f
eddiek001 <worksofeddy@gmail.com>
2023-10-20 09:54:48 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-int len(const char *str);
 
 /**
  * add_node_end - adds a new number to linker
@@ -13,41 +12,16 @@ int len(const char *str);
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int i, count = o;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	if (*head == NULL)
-
-		new_node->next = NULL;
-	else
-			new_node->next = *head;
-		new_node->str = strdup(str);
-		new_node->len = len(str);
-		*head = new_node;
-	return (*head);
-}
-/**
- * len - get lenght of the string
- * @str: the string
- * Return: length of the string
- */
-int len(const char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (0);
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	new_node->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	new_node->len = count;
+	new_node->next = *head;
+	*head = new_node;
+		return (*head);
 }
