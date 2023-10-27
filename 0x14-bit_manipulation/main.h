13711e8ac895563d7df15e246f76c38948a8079b
eddiek001 <worksofeddy@gmail.com>
2023-10-27 18:07:32 +0000

    set bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
