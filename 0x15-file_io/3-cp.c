10fe902582ce90479aedee1cf8dc66ef8f36af2f
eddiek001 <worksofeddy@gmail.com>
2023-10-31 09:10:32 +0000

    try 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 664);
 	error_file(file_from, file_to, argv);
 
 	nchar = 1024;
