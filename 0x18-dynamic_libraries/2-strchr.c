a1f20d9daab603c4d531cd87f3125f5c6ec55d75
eddiek001 <worksofeddy@gmail.com>
2023-12-12 23:10:44 +0300

    dynamic linking

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,19 +1,22 @@
 #include "main.h"
+
 /**
- * _strchr - main file
- * @s: input
- * @c: input
- * Return: 0
+ * _strchr - locates string character
+ * @s: pointer to s
+ * @c: character in the string
+ * Return: character
  */
+
 char *_strchr(char *s, char c)
 {
-	while (*s >= '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		s++;
 	}
-	return (0);
+	if (*s == c)
+		return (s);
+	return (NULL);
+
 }
